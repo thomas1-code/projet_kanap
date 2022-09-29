8cf6da31f9d19637f49427a8c606fef4fe150032
thomas1-code <thomas.o1@laposte.net>
2022-09-29 08:36:28 +0200

    refactorisation, modification de la fonction de changement de quantité

--- front/js/cart.js
+++ front/js/cart.js
@@ -3,120 +3,114 @@ function getCart(){
     return JSON.parse(localStorage.getItem("products"));
 }
 
-// 
-/**
- * Send a request using the fecth api to retrieve information from a product
- * @param { (String | Array | Integer) } localStorage 
- */
-function requestIdFetch(localStorage,){
-    const urlApi = "http://localhost:3000/api/products/" + localStorage.productId;
-    console.log(urlApi);
-    fetch(urlApi)
-        .then(function(res){
-            if (res.ok){
-                return res.json();
-        }
-        })
-        .then(function(data){
-            creationOfElements(localStorage, data);
-        })
-        .catch(function(err){
-            console.log(err);
-        })
-}
-
-/**
- * Dynamic creation of cart elements
- * @param { (String | Array | Integer) } localStorage 
- * @param { (String | Array | Integer) } api
- */
- function creationOfElements(localStorage, api){
-    const elementsCart = `
-        <article class="cart__item" data-id="${localStorage.productId}" data-color="${localStorage.productColor}">
-            <div class="cart__item__img">
-                <img src="${api.imageUrl}" alt="${api.altTxt}">
-            </div>
-            <div class="cart__item__content">
-                <div class="cart__item__content__description">
-                <h2>${api.name}</h2>
-                <p>${localStorage.productColor}</p>
-                <p>${api.price} €</p>
-                </div>
-                <div class="cart__item__content__settings">
-                <div class="cart__item__content__settings__quantity">
-                    <p>Qté : ${localStorage.productQuantity}</p>
-                    <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${localStorage.productQuantity}">
-                </div>
-                <div class="cart__item__content__settings__delete">
-                    <p class="deleteItem">Supprimer</p>
-                </div>
-                </div>
-            </div>
-        </article>`;
-        document.querySelector("#cart__items").insertAdjacentHTML("beforeend", elementsCart);
-        addTotalQuantity(localStorage);
-        addTotalPrice(localStorage, api);
-}
-
-// Save basket quantity
-const saveCartQuantity = [];
-
-// Calculation of total quantity
-function addTotalQuantity(local){
-    saveCartQuantity.push(local.productQuantity * 1);
-    const reducer = (previousValue, currentValue) => previousValue + currentValue;
-    const totalQuantity = saveCartQuantity.reduce(reducer);
-    document.querySelector("#totalQuantity").innerHTML = `${totalQuantity}`;
-}
-// Save basket prices
-const saveCartPrice = [];
-
-// Calculation of the total 
-function addTotalPrice(local, api){
-    saveCartPrice.push(local.productQuantity * api.price);
-    const reducer = (previousValue, currentValue) => previousValue + currentValue;
-    const totalPrice = saveCartPrice.reduce(reducer);
-    document.querySelector("#totalPrice").innerHTML = `${totalPrice}`;
-}
-
-// Dynamic integration of elements in the basket
-function addElements(){
-    if (getCart() === null){
+// Adding user selected items to cart
+function addItemsToCart(){
+    if (getCart() === null || getCart() == 0){
         document.querySelector("#cartAndFormContainer > h1").insertAdjacentText("beforeend", " est vide");
     }else{
         for (let i = 0; i < getCart().length; i++){
-            requestIdFetch(getCart()[i]);
+            fetch("http://localhost:3000/api/products/" + getCart()[i].productId)
+                .then(res => res.json())
+                .then(api =>{
+                    const elementsCart = `
+                    <article class="cart__item" data-id="${getCart()[i].productId}" data-color="${getCart()[i].productColor}">
+                        <div class="cart__item__img">
+                            <img src="${api.imageUrl}" alt="${api.altTxt}">
+                        </div>
+                        <div class="cart__item__content">
+                            <div class="cart__item__content__description">
+                            <h2>${api.name}</h2>
+                            <p>${getCart()[i].productColor}</p>
+                            <p>${api.price} €</p>
+                            </div>
+                            <div class="cart__item__content__settings">
+                            <div class="cart__item__content__settings__quantity">
+                                <p>Qté : ${getCart()[i].productQuantity}</p>
+                                <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${getCart()[i].productQuantity}">
+                            </div>
+                            <div class="cart__item__content__settings__delete">
+                                <p class="deleteItem">Supprimer</p>
+                            </div>
+                            </div>
+                        </div>
+                    </article>`;
+                    document.querySelector("#cart__items").insertAdjacentHTML("beforeend", elementsCart);
+                    const changeQuantity = document.querySelectorAll(".itemQuantity");
+                    for (let change of changeQuantity) {
+                        change.addEventListener("change", (e) =>{
+                            changeProductQuantity(e);
+                        })
+                    }
+                    const removeBtn = document.querySelectorAll(".deleteItem");
+                    for (let remove of removeBtn){
+                        remove.addEventListener("click", (e) =>{
+                            removeProduct(e);
+                        })
+                    }
+                })
+                .catch(err => console.log(err))
         }
-        setTimeout(() => {changeProductQuantity()}, 1000);
-        // changeProductQuantity();
     }
 }
 
-addElements();
+addItemsToCart();
+
+// Calculation of total quantity
+function addTotalQuantity(){
+    let quantityTotal = 0;
+    if (getCart() != null){
+        for (let i = 0 ; i < getCart().length; i++){
+            quantityTotal += getCart()[i].productQuantity * 1;
+        }
+        document.querySelector("#totalQuantity").innerHTML = quantityTotal;
+    }
+}
+
+addTotalQuantity();
+
+// Calculation of the total price
+function addTotalPrice(){
+    let totalPrice = 0;
+    if (getCart() != null){
+        for (let i = 0; i < getCart().length; i++){
+            fetch("http://localhost:3000/api/products/" + getCart()[i].productId)
+                .then(res => res.json())
+                .then(data =>{
+                totalPrice += getCart()[i].productQuantity * data.price;
+                document.querySelector("#totalPrice").innerHTML = totalPrice;
+                })
+                .catch(err => console.log(err))
+        }
+    }
+}
+
+addTotalPrice();
 
 // Save the cart in the localStorage
 function saveCart(cart){
     return localStorage.setItem("products", JSON.stringify(cart));
 }
 
-// Change the quantity of the product in the basket
-function changeProductQuantity(){
-    const changeQuantitys = document.querySelectorAll(".itemQuantity");
-    console.log(changeQuantitys);
-    changeQuantitys.forEach(changeQuantity => 
-        changeQuantity.addEventListener("change", (e) => {
-            const searchProduct = e.target.closest("article");
-            const quantityProduct = e.target.closest(".itemQuantity");
-            let cart = getCart();
-            if (quantityProduct.value <= 0){
-                quantityProduct.value = 1;
-                saveCart(cart);
-            }else{
-                let findProduct = cart.find(product => product.productId == searchProduct.dataset.id && product.productColor == searchProduct.dataset.color);
-                let newQuantity = changeQuantity.value;
-                findProduct.productQuantity = newQuantity;
-                console.log(findProduct);
-                saveCart(cart);
-            }
-    }));
+// Reloading the resource from the current URL
+function reloadPage(){
+    return window.location.reload()
 }
+
+// Change the quantity of the product in the basket
+function changeProductQuantity(e){
+    const searchProduct = e.target.closest("article");
+    const quantityProduct = e.target.closest(".itemQuantity");
+    let cart = getCart();
+    if (quantityProduct.value <= 0){
+        quantityProduct.value = 1;
+    }
+    if (quantityProduct.value >= 1){
+        let findProduct = cart.find(product => product.productId == searchProduct.dataset.id && product.productColor == searchProduct.dataset.color);
+        let newQuantity = quantityProduct.value;
+        findProduct.productQuantity = newQuantity;
+        console.log(findProduct);
+    }
+    saveCart(cart);
+    reloadPage();
+}
+
