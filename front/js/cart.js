541355c8cd318401c4f1f235be14a85ffa6958fd
thomas1-code <thomas.o1@laposte.net>
2022-09-29 08:41:30 +0200

    ajout fonction validation des données, création objet données commande

--- front/js/cart.js
+++ front/js/cart.js
@@ -122,3 +122,132 @@ function removeProduct(e){
     saveCart(cart);
     reloadPage();
 }
+
+// Checking the information entered in the form
+function validationInput(){
+    const input = document.querySelector(".cart__order__form");
+    input.firstName.addEventListener("change", function (){
+        validateFirstName(this);
+    })
+    input.lastName.addEventListener("change", function (){
+        validateLastName(this);
+    })
+    input.address.addEventListener("change", function (){
+        validateAddress(this);
+    })
+    input.city.addEventListener("change", function (){
+        validateCity(this);
+    })
+    input.email.addEventListener("change", function (){
+        validateEmail(this);
+    })
+}
+
+validationInput();
+
+/**
+ * Validation of the data entered for the first name
+ * @param {(String)} inputFirstName 
+ * @returns 
+ */
+function validateFirstName(inputFirstName){
+    const errorMsgFirstName = document.querySelector("#firstNameErrorMsg");
+    const regexName = new RegExp("^[a-zA-Zé'è`çà¨^ù.\-]{2,25}$");
+    if (regexName.test(inputFirstName.value)){
+        errorMsgFirstName.innerText = "";
+        return true
+    }else{
+        errorMsgFirstName.innerText = "Veuillez entrer un prénom valide (ex : Jean ou jean-marie)";
+        return false
+    }
+}
+
+/**
+ * Validation of the data entered for the last name
+ * @param { (String) } inputLastName 
+ * @returns 
+ */
+function validateLastName(inputLastName){
+    const errorMsgLastName = document.querySelector("#lastNameErrorMsg");
+    const regexName = new RegExp("^[a-zA-Zé'è`çà¨^ù.\-]{2,25}$");
+    if (regexName.test(inputLastName.value)){
+        errorMsgLastName.innerText = "";
+        return true 
+    }else{
+        errorMsgLastName.innerText = "Veuillez entrer un nom valide (ex : D'arc ou dupont)";
+        return false
+    }
+}
+
+/**
+ * Validation of the data entered for the address
+ * @param { ((String)) } inputAddress
+ * @returns 
+ */
+function validateAddress(inputAddress){
+    const errorMsgAddress = document.querySelector("#addressErrorMsg");
+    const regexAddress = new RegExp("^[a-zA-Zé'è`çà¨^ù.,0-9., \-]{5,75}$");
+     if (regexAddress.test(inputAddress.value)){
+        errorMsgAddress.innerText = "";
+        return true
+    }else{
+        errorMsgAddress.innerText = "Veuillez entrer une adresse valide (ex : 15 bd de la République)";
+        return false        
+    }
+}
+
+/**
+ * Validation of the data entered for the city
+ * @param { (String | Integer) } inputCity 
+ * @returns 
+ */
+function validateCity(inputCity){
+    const errorMsgCity = document.querySelector("#cityErrorMsg");
+    const regexCity = new RegExp("^[a-zA-Zé'è`çà¨^ù.\-]{3,30}$");
+    if (regexCity.test(inputCity.value)){
+        errorMsgCity.innerText = "";
+        return true
+    }else{
+        errorMsgCity.innerText = "Veuillez entrer une ville valide (ex : Paris ou nantes)";
+        return false
+    }
+}
+
+/**
+ * Validation of the data entered for the email
+ * @param { (String | Integer) } inputEmail 
+ * @returns 
+ */
+function validateEmail(inputEmail){
+    const errorMsgEmail = document.querySelector("#emailErrorMsg");
+    const regexEmail =  /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    if (regexEmail.test(inputEmail.value)){
+        errorMsgEmail.innerText = "";
+        return true 
+    }else{
+        errorMsgEmail.innerText = "Veuillez entrer un email valide (ex : monemail@exemple.fr)";
+        return false
+    }
+}
+
+// Storage of information entered in the form
+function storeOrderData(){
+    const productId = [];
+    if (getCart() != null){
+        for (let i = 0; i < getCart().length; i++){
+            productId.push(getCart()[i].productId)
+        }
+    }
+    const order = {
+            contact:{
+                firstName: firstName.value,
+                lastName: lastName.value,
+                address: address.value,
+                city: city.value,
+                email: email.value
+            },
+            products: productId
+            
+    }
+    return order
+}
