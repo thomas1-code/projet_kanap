5cafbb00d9798c768dedad745cb0c48b3fbd79b4
thomas1-code <thomas.o1@laposte.net>
2022-10-03 08:45:06 +0200

    ajout des commentaires

--- front/js/confirmation.js
+++ front/js/confirmation.js
@@ -1,9 +1,9 @@
-// 
+// Retrieve the order number
 function getProductId(){
     return new URL(location.href).searchParams.get("orderId");
   }
 
-// 
+// Display the order number
 function showOrderValidation(){
   document.querySelector("#orderId").innerHTML = getProductId();
 }
